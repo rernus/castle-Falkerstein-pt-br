93046bb7325ebb61027a63f35d7e5cb83fe1e94e
rernus <robertaguiar037@gmail.com>
2025-02-07 18:24:10 -0300

    4,0,1

--- babele-register.js
+++ babele-register.js
@@ -1,7 +1,7 @@
 Hooks.once('init', () => { 
 	if(typeof Babele !== 'undefined') { 
 		Babele.get().register({
-			module: 'castle-Falkerstein',
+			module: 'castle-falkerstein',
 			lang: 'pt-BR',
 			dir: 'compendium'
 		}); 
